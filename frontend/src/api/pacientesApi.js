f5681cfc2b72f2c4b620ebe88d7c9d28813e85d3
EYDERAVCRISTIANBU <eyderarroyo19@gmail.com>
2026-04-07 19:16:16 -0500

    Relación con el Modelo Backend

--- frontend/src/api/pacientesApi.js
+++ frontend/src/api/pacientesApi.js
@@ -23,7 +23,7 @@ export async function buscarPacientePorDocumento(numeroIdentificacion) {
 }
   */
 
-const BASE_URL = "http://localhost:4000/api/pacientes";
+/*const BASE_URL = "http://localhost:4000/api/pacientes";
 
 export async function crearPaciente(data) {
   const response = await fetch(BASE_URL, {
@@ -44,5 +44,19 @@ export async function obtenerPacientes() {
 
 export async function buscarPacientePorDocumento(numeroIdentificacion) {
   const response = await fetch(`${BASE_URL}/documento/${numeroIdentificacion}`);
+  return await response.json();
+}
+*/
+const BASE_URL = "http://localhost:4000/api/pacientes";
+
+export async function crearPaciente(data) {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
   return await response.json();
 }
