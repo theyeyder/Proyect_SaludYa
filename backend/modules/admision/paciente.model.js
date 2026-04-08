83e1b21a041a90b8f32cd5d2bf11634624e73ab7
CristianBacca217 <Cristianbacca217@gmail.com>
2026-04-07 19:11:53 -0500

    Update paciente.model.js

--- backend/modules/admision/paciente.model.js
+++ backend/modules/admision/paciente.model.js
@@ -1,1 +1,24 @@
+const mongoose = require("mongoose");
 
+const PacienteSchema = new mongoose.Schema(
+  {
+    nombres: { type: String, required: true },
+    tipoIdentificacion: { type: String, required: true },
+    numeroIdentificacion: { type: String, required: true, unique: true },
+    ciudad: { type: String, required: true },
+    direccion: { type: String, required: true },
+    fechaNacimiento: { type: String, required: true },
+    sintomas: { type: String, default: "" },
+    alergico: { type: String, enum: ["Si", "No"], default: "No" },
+    acompanante: { type: String, default: "" },
+    sexo: {
+      type: String,
+      enum: ["Masculino", "Femenino", "Otro"],
+      required: true,
+    },
+    estado: { type: Boolean, default: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Paciente", PacienteSchema);
